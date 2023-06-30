23576e15f8608c84a2ac8106ddbf3629c48ba0e6
ashrit kankipati <akankip1@binghamton.edu>
2023-06-30 16:21:19 -0400

    discard button

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,6 +1,7 @@
 import Modal from '../components/Modal';
 import '../components/Post.css'
 import { useState } from "react";
+import { Link, NavLink } from 'react-router-dom';
 export default function NewPost(props){
     const [bodytext,setBodyText]=useState('');
     const [nametext,setNameText]=useState('');
@@ -33,10 +34,16 @@ export default function NewPost(props){
             <label htmlFor="name">Name</label>
             <input type="text" id='name' required onChange={changename}/>            
             </p>
-            <p className='actions'>
-                <button type='submit'>Post</button>
-                <button type='button'onClick={props.onCancel}>Discard</button>
-            </p> 
+            <div className='actions'>
+                <button type='submit'>
+                    Post
+                </button>
+                <Link to='..'>
+                <button type='button'>
+                    Discard
+                </button>
+                </Link>
+            </div> 
         </form>
         </Modal>
     )
