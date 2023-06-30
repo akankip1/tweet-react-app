c0ba635b2f43ec171d4c8bbfe1e5bf37c468ba72
ashrit kankipati <akankip1@binghamton.edu>
2023-06-30 15:46:27 -0400

    useNavigate for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,13 @@
 import './Modal.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function Modal(props){
+    const navigate=useNavigate();
+    function closeHandler(){
+        navigate('..');
+    }
     return(<>
-    <div className='backdrop' onClick={props.hideModal}/>
+    <div className='backdrop' onClick={closeHandler}/>
     <dialog open className='modal'>{props.children}</dialog>
     
     
